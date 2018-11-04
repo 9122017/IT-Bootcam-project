c2fd3614586892a50033dceba560a2b96f20785a
Katarina <katprokic@gmail.com>
2018-11-04 10:31:46 +0100

    first commit

---
+++ pr_katarina_prokic/src/utility/Constant.java
@@ -0,0 +1,20 @@
+package utility;
+
+public class Constant {
+
+	public static final String URL = "http://app.invoice-factory.source-code.rs/login";
+
+	public static final String Email = "katprokic@gmail.com";
+
+	public static final String Password = "qwe123";
+
+	public static final String Path_TestData = "C:\\Users\\Mina\\Desktop\\";
+
+	public static final String File_TestData = "TestData.xlsx";
+
+	public static final String URLI = "http://app.invoice-factory.source-code.rs/invoices";
+
+	public static final String URLB = "http://app.invoice-factory.source-code.rs/business";
+
+	public static final String URLC = "http://app.invoice-factory.source-code.rs/clients";
+}
